2f39a4b22efc9d2859797a81a67078314da30cfd
kryptiksage <kryptiksage@gmail.com>
2021-05-03 19:58:21 +0530

    src/App : added module css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Preview from "./components/Preview";
+import styles from "./App.module.css";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello</h1>
+		<div className={styles.container}>
+			<h1>Markdown Preview</h1>
 			<Preview />
 		</div>
 	);
