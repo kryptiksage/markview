4043d2156d69acc239bb72e031750e7cd51d00bb
kryptiksage <kryptiksage@gmail.com>
2021-05-03 19:52:44 +0530

    src/components/Preview : added textarea

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,7 +1,15 @@
 import styles from "./Preview.module.css";
 
 const Preview = () => {
-	return <div className={styles.container}></div>;
+	return (
+		<div className={styles.container}>
+			<textarea
+				name="md_input"
+				id="md_input"
+				className={styles.md_input}
+			></textarea>
+		</div>
+	);
 };
 
 export default Preview;
