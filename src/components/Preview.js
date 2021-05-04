56a3396a895e51726af7c0049f684e9d67e949ee
kryptiksage <kryptiksage@gmail.com>
2021-05-04 16:08:39 +0530

    src/components/Preview.js : added syntax highlighting support for codeblocks

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,13 +1,32 @@
 import { useState } from "react";
 import styles from "./Preview.module.css";
 import ReactMarkdown from "react-markdown";
-import { render } from "react-dom";
+
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialOceanic } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const Preview = () => {
 	const [markdown, setMarkdown] = useState("Preview here");
 
 	const markdownChange = (e) => {
 		setMarkdown(e.target.value);
+	};
+
+	const components = {
+		code({ node, inline, className, children, ...props }) {
+			const match = /language-(\w+)/.exec(className || "");
+			return !inline && match ? (
+				<SyntaxHighlighter
+					style={materialOceanic}
+					language={match[1]}
+					PreTag="div"
+					children={String(children).replace(/\n$/, "")}
+					{...props}
+				/>
+			) : (
+				<code className={className} {...props} />
+			);
+		},
 	};
 
 	return (
@@ -25,7 +44,12 @@ const Preview = () => {
 			<div className={styles.preview}>
 				<h3>Preview</h3>
 				<div className={styles.md_preview}>
-					<ReactMarkdown>{markdown}</ReactMarkdown>
+					<ReactMarkdown
+						components={components}
+						style={materialOceanic}
+					>
+						{markdown}
+					</ReactMarkdown>
 				</div>
 			</div>
 		</div>
