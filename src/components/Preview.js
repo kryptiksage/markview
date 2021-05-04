35e6f8a0092db69479d4ee81dbf5ad43edaf4a64
kryptiksage <kryptiksage@gmail.com>
2021-05-04 12:16:24 +0530

    src/components/Preview : added flex styling to divs

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,14 +3,25 @@ import styles from "./Preview.module.css";
 const Preview = () => {
 	return (
 		<div className={styles.container}>
-			<textarea
-				name="md_input"
-				id="md_input"
-				className={styles.md_input}
-			></textarea>
-			<textarea name="" id="" className={styles.md_preview} readOnly>
-				Hello
-			</textarea>
+			<div className={styles.markdown}>
+				<h3>Markdown</h3>
+				<textarea
+					name="md_input"
+					id="md_input"
+					className={styles.md_input}
+					placeholder="Type here..."
+				></textarea>
+			</div>
+			<div className={styles.preview}>
+				<h3>Preview</h3>
+				<textarea
+					name=""
+					id=""
+					className={styles.md_preview}
+					readOnly
+					placeholder="Preview here"
+				></textarea>
+			</div>
 		</div>
 	);
 };
