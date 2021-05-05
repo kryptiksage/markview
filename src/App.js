10a6ba3eda0a4b0898e96c13ff0ade38b8b39d2a
kryptiksage <kryptiksage@gmail.com>
2021-05-05 12:03:57 +0530

    src/App.js : added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Preview from "./components/Preview";
+import Footer from "./components/Footer";
 import styles from "./App.module.css";
 
 function App() {
 	return (
 		<div className={styles.container}>
-			<h1>Markdown Preview</h1>
 			<Preview />
+			<Footer />
 		</div>
 	);
 }
